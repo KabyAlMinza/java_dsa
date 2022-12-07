90f8a7342b8d642b0e7968d92948b88f5b0de00e
Daniyal Ahmad <arifdaniyal334@gmail.com>
2022-12-07 15:29:09 +0530

    ceiling and floor done

---
+++ java_dsa/src/BSQ1.java
@@ -0,0 +1,25 @@
+public class BSQ1 {
+    public static void main(String[] args) {
+        int arr[] = {2,3,5,9,14,16,18};
+        int target = 15;
+        System.out.println(ceiling(arr, target));
+    }
+    static int ceiling(int arr[], int target ){
+        int min = 0;
+        int max = arr.length-1;
+        while (min<=max){
+            int mid = min + (max-min)/2;
+
+            if (target<arr[mid]){
+                max = mid-1;
+            }
+            else if (target > arr[mid]){
+                min = mid+1;
+            }
+            else{
+                return mid;
+            }
+        }
+    return arr[min];
+    }
+}
